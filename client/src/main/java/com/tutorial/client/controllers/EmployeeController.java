c2b314e3a90b177e71f01f126320be7e84c183e0
lorence <lorence@enclave.vn>
2018-09-21 13:51:36 +0700

    .

--- client/src/main/java/com/tutorial/client/controllers/EmployeeController.java
+++ client/src/main/java/com/tutorial/client/controllers/EmployeeController.java
@@ -1,9 +1,22 @@
 package com.tutorial.client.controllers;
 
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 @Controller
 public class EmployeeController {
@@ -11,6 +24,34 @@ public class EmployeeController {
 	@RequestMapping(value = "/getEmployees", method = RequestMethod.GET)
 	public ModelAndView getEmployeeInfo() {
 		return new ModelAndView("getEmployees");
+	}
+	
+	@RequestMapping(value = "/showEmployees", method = RequestMethod.GET)
+	public ModelAndView showEmployees(@RequestParam("code") String code) throws JsonProcessingException, IOException {
+		ResponseEntity<String> response = null;
+		System.out.println("Authorization Code------" + code);
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		String credentials = "javainuse:secret";
+		String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		headers.add("Authorization", "Basic " + encodedCredentials);
+
+		HttpEntity<String> request = new HttpEntity<String>(headers);
+
+		String access_token_url = "http://localhost:8080/oauth/token";
+		access_token_url += "?code=" + code;
+		access_token_url += "&grant_type=authorization_code";
+		access_token_url += "&redirect_uri=http://localhost:8090/showEmployees";
+
+		response = restTemplate.exchange(access_token_url, HttpMethod.POST, request, String.class);
+
+		System.out.println("Access Token Response ---------" + response.getBody());
+
+		return null;
 	}
 }
 
